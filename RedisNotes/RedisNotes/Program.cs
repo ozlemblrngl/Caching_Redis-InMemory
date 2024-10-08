eaa91027b371c3dc6e2bab4bdf740808d2f236b5
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-08 17:26:14 +0300

    In Memory Caching and Distributed Caching notes added

---
+++ RedisNotes/RedisNotes/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Caching Nedir?
+        // Çok sık kullandığımız dataların kaydedilmesi hem de ihtiyaç halinde okunması tekniğidir.
+        // ikiye ayrılır:
+
+        //    1- In-Memory Caching 
+        //    2- Distributed Caching
+
+        // In Memory Caching
+
+        // uygulamayla ilgili dataların, uygulamayı barındıran Web Server'ın ram'inde yani memory sinde kaydetme işlemidir. Ram'de tutulur data.
+        // Verilere hızlıca ulaşabilmek için Ram'de kaydedilir datalar.
+
+        // Distributed Caching
+
+        // Tamamen ayrı bir cach servisinde tutulur.
+        // cachelenmiş datalar ayrı serviste tutulacağı için, uygulama restart dahi olsa cacheler kaybolmaz.
+        // Ayrıca inmemory de olduğu gibi aynı verinin farklı versiyonlardan gelebilecek farklı veri kayıtlarının oluşturacağı tutarsızlıkların da burada önüne geçilir.
+        // dezavantajı daha yavaştır, in memory çok daha hızlıdır
+        // bir de implementasyonu in memory'e göre daha uğraştırıcıdır. 
+        // Ancak bunlar çok da büyük sorunlar değil keza veri tutarlılığı hızdan çok daha önemlidir.
+
+    }
+}
