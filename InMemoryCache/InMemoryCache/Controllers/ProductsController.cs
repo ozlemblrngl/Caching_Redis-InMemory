7fae79da4a6b6893b4ae932f4804354434477f95
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 10:41:08 +0300

    feature: IMemoryCache added to ProductsController

---
+++ InMemoryCache/InMemoryCache/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCache.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductsController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
