5969ad2e69f2e6dc7a638430351db733dc815c45
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 11:46:05 +0300

    feature: trygetvalue() and remove() method used

--- InMemoryCache/InMemoryCache/Controllers/ProductsController.cs
+++ InMemoryCache/InMemoryCache/Controllers/ProductsController.cs
@@ -12,12 +12,30 @@ namespace InMemoryCache.Controllers
         }
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //zaman key'ine sahip bir data var mı yok mu kontrolü için iki yol vardır:
+
+            //1. yol eğer zaman key i yoksa buna içeriği set et
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //2.yol
+            // eğer zaman key'i varsa bunun değerini al string timecache de dön. yoksa içeriği set et 
+            if (_memoryCache.TryGetValue("zaman", out string timecache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //eğer değer varsa timecache in değeri üzerinden devam edebiliriz.
+
             return View();
         }
 
         public IActionResult Show()
         {
+            // ilgili key'e sahip olan data'yı memory den silmek için Remove'u kullanıyoruz.
+            _memoryCache.Remove("zaman");
             ViewBag.time = _memoryCache.Get<string>("zaman");
             return View();
         }
