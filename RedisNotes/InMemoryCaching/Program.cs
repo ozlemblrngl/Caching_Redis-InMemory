a34b15277e84c80af1ad568a43d65cbb01ec6ce9
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 10:56:21 +0300

    fix: get set method notes added

--- RedisNotes/InMemoryCaching/Program.cs
+++ RedisNotes/InMemoryCaching/Program.cs
@@ -5,5 +5,8 @@
         //AddMemoryCache() servis ve IMemoryCache interface
         // InMemory Cache i kullanacaksak öncelikle memorycache servisini eklememiz lazım.
         // Akabinde nerede kullanacaksak ilgili sınıfın constructorında IMemoryCache interface i dependency injection yoluyla kullanmamız gerekiyor.
+
+        // InMemoryCache Get() --> memoryden bir data almak için ve Set() ---> memory ye bir data yazmak için.
+        // Bunlar aynı zamanda generic metotlardır, resim, döküman, pdf, ya da bir sınıfı serialize ederek de tutabiliriz.Tek sınırımız bizim memorydeki hafıza miktarımızdır. Yeter ki serialize edelim.
     }
 }
