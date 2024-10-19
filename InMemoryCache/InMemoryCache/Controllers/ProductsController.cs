33e0375b90932295332079ab11943aee410351f7
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 12:34:54 +0300

    fix: adding to absolutetimeexpiration

--- InMemoryCache/InMemoryCache/Controllers/ProductsController.cs
+++ InMemoryCache/InMemoryCache/Controllers/ProductsController.cs
@@ -12,39 +12,23 @@ namespace InMemoryCache.Controllers
         }
         public IActionResult Index()
         {
-            //zaman key'ine sahip bir data var mı yok mu kontrolü için iki yol vardır:
 
-            //1. yol eğer zaman key i yoksa buna içeriği set et
-            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            // ömür vermek için kullandığımız sınıf
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
 
-            //2.yol
-            // eğer zaman key'i varsa bunun değerini al string timecache de dön. yoksa içeriği set et 
-            if (_memoryCache.TryGetValue("zaman", out string timecache))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // 10 sn lik ömür vermiş olduk.
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
-            //eğer değer varsa timecache in değeri üzerinden devam edebiliriz.
+
 
             return View();
         }
 
         public IActionResult Show()
         {
-            // ilgili key'e sahip olan data'yı memory den silmek için Remove'u kullanıyoruz.
-            _memoryCache.Remove("zaman");
+            _memoryCache.TryGetValue("zaman", out string timecache);
 
-            // GetOrCreate() ilgili keya sahip data varsa getirir yoksa oluşturur.Bu metotla hiç var mı yok mu teker teker uğraşmadan işlemleri yapıyoruz.
-            // entry nin gelmesinin sebebi key dışında başka özellik vs set etmek istiyorsak diye geliyor.
-            _memoryCache.GetOrCreate<string>("zaman", entry =>
-            {
-                //entry. yazınca çıkıyor tüm ekstra özellikler
-                return DateTime.Now.ToString();
-            });
-            ViewBag.time = _memoryCache.Get<string>("zaman");
+            ViewBag.time = timecache;
             return View();
         }
     }
