452c6a8ee1ac79c751992ce13a00794ed463687b
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 10:00:58 +0300

    InMemoryCaching

---
+++ RedisNotes/InMemoryCaching/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //AddMemoryCache() servis ve IMemoryCache interface
+        // InMemory Cache i kullanacaksak öncelikle memorycache servisini eklememiz lazım.
+        // Akabinde nerede kullanacaksak ilgili sınıfın constructorında IMemoryCache interface i dependency injection yoluyla kullanmamız gerekiyor.
+    }
+}
